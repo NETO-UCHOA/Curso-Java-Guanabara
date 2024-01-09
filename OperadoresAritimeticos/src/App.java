fb5a648e93acf3a2f6cbc0bcbc6c7c3fead10bfc
Neto Uchoa <NetoJetWork@gmail.com>
2024-01-09 10:32:33 -0300

    estudo 09-01-24

---
+++ OperadoresAritimeticos/src/App.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+      int n1 = 3;
+      int n2 = 5;
+      float m = (n1 + n2)/2;
+      System.out.println("A média entre 3 e 5 é:"+ m);  
+
+        int numero = 5;
+        numero ++;
+        System.out.println(numero);
+
+        
+        int valor  = 5 + numero ++;
+        System.out.println(valor);
+
+        int a = 2;
+        int b = 5;
+        int d = b /a;
+        int r = b % a;
+        System.out.println(d);
+        System.out.println(r);
+        
+
+    }
+}
